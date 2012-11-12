f990f2b50e9e6aeb50b22155700296f18f1a67fb
William Gillis <rechelon@gmail.com>
2012-11-12 15:07:19 -0800

    adding tests to theme model

---
+++ spec/models/theme_spec.rb
@@ -0,0 +1,28 @@
+require File.dirname(__FILE__) + '/../spec_helper.rb'
+
+describe Theme do
+  before do
+    @theme = create_theme
+  end
+
+  describe "should parse the body" do
+    before do
+      create_theme_element :name => "body", :markdown => "testing one two{{content}} three", :theme => @theme
+    end
+
+    it "into pre_content" do
+      @theme.pre_content.should == "testing one two"
+    end
+    
+    it "into post_content" do
+      @theme.post_content.should == " three"
+    end
+  end
+
+  it "should respond to theme.element and theme.element=" do
+    Theme::THEME_ELEMENT_NAMES.each do |e|
+      @theme.should respond_to(e)
+      @theme.should respond_to(e+'=')
+    end
+  end
+end
