99850b259141bd204e77a418fca762d53b2082b5
William Gillis <rechelon@gmail.com>
2012-12-25 12:11:13 +0100

    changing spec/models/varnish/theme_element_sweeper to factory_girl

--- spec/models/varnish/theme_element_sweeper_spec.rb
+++ spec/models/varnish/theme_element_sweeper_spec.rb
@@ -2,29 +2,29 @@ require File.dirname(__FILE__) + '/../../spec_helper'
 
 describe Varnish::ThemeElementSweeper do
   before do
-    Site.stub!(:current).and_return new_site
-    Host.stub!(:current).and_return new_host
+    Site.stub!(:current).and_return(build :site)
+    Host.stub!(:current).and_return(build :host)
     Site.current.hosts << Host.current
-    @theme = create_theme
-    @calendar_one = create_calendar(:permalink => "one", :theme_id => @theme.id)
-    @calendar_two = create_calendar(:permalink => "two", :theme_id => @theme.id)
+    @theme = create :theme
+    @calendar_one = create :calendar, :permalink => "one", :theme_id => @theme.id
+    @calendar_two = create :calendar, :permalink => "two", :theme_id => @theme.id
   end
 
   describe "when a theme element is created" do
     it "should determine the correct purge routes" do
       Varnish::ThemeElementSweeper.instance.should_receive(:purges).with(['one','two']).and_return([])
       Varnish::ThemeElementSweeper.instance.should_receive(:bans).with(['one/','two/']).and_return([])
-      create_theme_element :theme => @theme
+      create :theme_element, :theme => @theme
     end
     it "should call the purging method" do
       Varnish::ThemeElementSweeper.instance.should_receive(:hydra_run_requests)
-      create_theme_element :theme => @theme
+      create :theme_element, :theme => @theme
     end
   end
 
   describe "when an event" do
     before do
-      @theme_element = create_theme_element :theme => @theme
+      @theme_element = create :theme_element, :theme => @theme
     end
     
     describe "is updated" do
