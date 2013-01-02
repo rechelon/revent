09dbf438ac417b9004ba83ded72747790fa1a13c
William Gillis <rechelon@gmail.com>
2013-01-02 20:16:39 +0100

    bringing user_mailer_spec into like with factory_girl

--- spec/models/user_mailer_spec.rb
+++ spec/models/user_mailer_spec.rb
@@ -3,19 +3,19 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe UserMailer do
   before do
     Site.current = Site.new(:host => 'revent.local')
-    @user = new_user
+    @user = build :user
     initialize_site
   end 
 
   it "uses the calendar admin email" do
-    Site.current.stub!(:calendars).and_return([new_calendar(:admin_email => 'info@radicaldesigns.org')])
+    Site.current.stub!(:calendars).and_return([build :calendar, :admin_email => 'info@radicaldesigns.org'])
     response = UserMailer.create_activation(@user)
     assert_equal('info@radicaldesigns.org', response.from[0])
   end
   
   describe "events in time" do
     before do
-      @event = new_event(:id => 333, :name => 'Step It Up Rally', :city => 'San Francisco', :state => 'CA', :location => '1370 Mission St., 4th Fl.', :start => 1.day.ago, :end => (1.day.ago + 2.hours))
+      @event = build :event, :id => 333, :name => 'Step It Up Rally', :city => 'San Francisco', :state => 'CA', :location => '1370 Mission St., 4th Fl.', :start => 1.day.ago, :end => (1.day.ago + 2.hours)
       @message = {:subject => 'check, check, testing, 1, 2, 3', :body => 'test upcoming/past events'}
     end
     it "knows if event is in the past" do
