c44c3601ff6a10065c2adaffaf303ed9ecdcbe74
William Gillis <rechelon@gmail.com>
2013-02-06 18:39:17 -0800

    memcache_util library should use dalli

--- lib/memcache_util.rb
+++ lib/memcache_util.rb
@@ -1,21 +1,7 @@
-##
-# A utility wrapper around the MemCache client to simplify cache access.  All
-# methods silently ignore MemCache errors.
+# A utility wrapper around the Dalli client to simplify cache access.  All
+# methods silently ignore Dalli errors.
 
 module Cache
-  
-  ##
-  # Try to return a logger object that does not rely
-  # on ActiveRecord for logging.
-  def self.logger
-    @logger ||= if defined? Rails.logger # Rails 2.1 +
-      Rails.logger
-    elsif defined? RAILS_DEFAULT_LOGGER # Rails 1.2.2 +
-      RAILS_DEFAULT_LOGGER
-    else
-      ActiveRecord::Base.logger # ... very old Rails.
-    end
-  end
   ##
   # Returns the object at +key+ from the cache if successful, or nil if either
   # the object is not in the cache or if there was an error attermpting to
@@ -27,16 +13,16 @@ module Cache
 
   def self.get(key, expiry = 0)
     start_time = Time.now
-    value = CACHE.get key
+    value = DC.get key
     elapsed = Time.now - start_time
-    logger.debug('MemCache Get (%0.6f)  %s' % [elapsed, key])
+    Rails.logger.debug('MemCache Get (%0.6f)  %s' % [elapsed, key])
     if value.nil? and block_given? then
       value = yield
       add key, value, expiry
     end
     value
-  rescue MemCache::MemCacheError => err
-    logger.debug "MemCache Error: #{err.message}"
+  rescue Dalli::RingError => err
+    Rails.logger.debug "MemCache Error: #{err.message}"
     if block_given? then
       value = yield
       put key, value, expiry
@@ -50,11 +36,11 @@ module Cache
 
   def self.put(key, value, expiry = 0)
     start_time = Time.now
-    CACHE.set key, value, expiry
+    DC.set key, value, expiry
     elapsed = Time.now - start_time
-    logger.debug('MemCache Set (%0.6f)  %s' % [elapsed, key])
+    Rails.logger.debug('MemCache Set (%0.6f)  %s' % [elapsed, key])
     value
-  rescue MemCache::MemCacheError => err
+  rescue Dalli::RingError => err
     ActiveRecord::Base.logger.debug "MemCache Error: #{err.message}"
     nil
   end
@@ -65,11 +51,11 @@ module Cache
 
   def self.add(key, value, expiry = 0)
     start_time = Time.now
-    response = CACHE.add key, value, expiry
+    response = DC.add key, value, expiry
     elapsed = Time.now - start_time
-    logger.debug('MemCache Add (%0.6f)  %s' % [elapsed, key])
+    Rails.logger.debug('MemCache Add (%0.6f)  %s' % [elapsed, key])
     (response == "STORED\r\n") ? value : nil
-  rescue MemCache::MemCacheError => err
+  rescue Dalli::RingError => err
     ActiveRecord::Base.logger.debug "MemCache Error: #{err.message}"
     nil
   end
@@ -79,22 +65,13 @@ module Cache
 
   def self.delete(key, delay = nil)
     start_time = Time.now
-    CACHE.delete key, delay
+    DC.delete key, delay
     elapsed = Time.now - start_time
-    logger.debug('MemCache Delete (%0.6f)  %s' %
+    Rails.logger.debug('MemCache Delete (%0.6f)  %s' %
                                     [elapsed, key])
     nil
-  rescue MemCache::MemCacheError => err
-    logger.debug "MemCache Error: #{err.message}"
-    nil
-  end
-
-  ##
-  # Resets all connections to MemCache servers.
-
-  def self.reset
-    CACHE.reset
-    logger.debug 'MemCache Connections Reset'
+  rescue Dalli::RingError => err
+    Rails.logger.debug "MemCache Error: #{err.message}"
     nil
   end
 
