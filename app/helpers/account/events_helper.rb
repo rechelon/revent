01f6ecc3a4c6630ba118ef2b4b63e90ee4d722f8
William Gillis <rechelon@gmail.com>
2013-02-07 21:43:56 -0800

    adding html_safe to views/helpers

--- app/helpers/account/events_helper.rb
+++ app/helpers/account/events_helper.rb
@@ -5,7 +5,7 @@ module Account::EventsHelper
     %|
     <div class="social-btn"><a id="twitter-btn" class="social-btn" target="_blank" onclick="return popup(this, 'twitter', 850, 500)" href="http://twitter.com/home?status=#{msg}"><img src="/themes/aflcio/images/Twitter_32x32.png" align="left" hspace="5" valign="absmiddle" border="0" /> <span class="social-text">#{link_text}</span></a>
     </div>
-    | 
+    |.html_safe 
   end
   
   def facebook_share(msg, title, link_text=false)
@@ -15,6 +15,6 @@ module Account::EventsHelper
     %|
     <div class="social-btn"><a id="facebook-btn" class="socal-btn" target="_blank" onclick="return popup(this, 'facebook', 560, 400)"  href="http://www.facebook.com/sharer.php?u=#{msg}&t=#{title}"><img src="/themes/aflcio/images/FaceBook_32x32.png" hspace="5" border="0" valign="left" align="left"/> <span class="social-text">#{link_text}</span></a>
     </div>     
-    |
+    |.html_safe
   end
 end
