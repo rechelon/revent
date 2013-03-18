4758da4a63effaa904370ae7297821bfb0803d03
William Gillis <rechelon@gmail.com>
2013-03-18 15:25:58 -0700

    adding 'select_field_with_group' helper, making us_states generated with this

--- public/javascripts/framework/helpers/select_field.js
+++ public/javascripts/framework/helpers/select_field.js
@@ -1,10 +1,40 @@
 function select_field(options,attr,selected_value){
   var options = _.clone(options);
+  var html = select_field_open_tag(attr);
+  html += select_field_options(options, selected_value);
+  html += '</select>';
+  return html;
+}
+
+function select_field_with_groups(options, attr, selected_value){
+  var options = _.clone(options);
+  var html = select_field_open_tag(attr);
+  _.each(options, function(value, text){
+    if(_.isString(value)){
+      var temp_field = {}
+      temp_field[text] = value;
+      html += select_field_options(temp_field, selected_value);
+    } else {
+      html += "<optgroup label='"+text+"'>";
+      html += select_field_options(value, selected_value);
+      html += "</optgroup>";
+    }
+  });
+  html += '</select>';
+  return html;
+}
+
+function select_field_open_tag(attr){
   var html = "<select ";
   _.each(attr,function(value,name){
     html += name+'="'+value+'" ';
   });
-  html += '>';
+  html += '>'; 
+  return html;
+}
+
+function select_field_options(options, selected_value){
+  var html = "";
   if(options['']){
     if(!_.isArray(options[''])){
       html += '<option value="">'+options['']+'</option>';
@@ -24,6 +54,5 @@ function select_field(options,attr,selected_value){
     }
     html += '<option value="'+value+'" '+selected+'>'+text+'</option>';
   });
-  html += '</select>';
   return html;
 }
