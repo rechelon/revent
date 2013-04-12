c5749eb260699bf6c3d40911e42a21d7464985c0
William Gillis <rechelon@gmail.com>
2013-04-11 18:35:44 -0700

    save_with_validation(false) to save(:validation => false)

--- app/models/user.rb
+++ app/models/user.rb
@@ -219,7 +219,7 @@ class User < ActiveRecord::Base
     end
     unless u.save
       logger.warn("Validation error(s) occurred when trying to create user from DemocracyInActionSupporter: #{u.errors.inspect}")
-      u.save_with_validation(false)
+      u.save :validation => false
     end
     dia_obj.synced = u
     dia_obj.save
