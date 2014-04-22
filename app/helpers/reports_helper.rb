70b7c421f91282e93728bf0e0cc01bad3ba38dd0
cooperq <cooperq@cooperq.com>
2014-04-21 18:27:18 -0700

    scope reportback selection to just events the user hosted or attended

--- app/helpers/reports_helper.rb
+++ app/helpers/reports_helper.rb
@@ -5,7 +5,9 @@ module ReportsHelper
     image ? image_tag(image.public_filename(:list)) : nil
   end
 
-  def events_select
-    @calendar.events.reportable.newer_than(@calendar.past_event_cutoff - 60.days).all(:order=>"state,city").collect {|e| [truncate("#{e.state || e.country} - #{e.city} - #{e.start.strftime('%m/%d/%y')}: #{e.name}",:length => 70), e.id]}
+  def events_select(user)
+    user.events.reportable.newer_than(@calendar.past_event_cutoff - 60.days).all(:order=>"state,city").collect {|e| [truncate("#{e.state || e.country} - #{e.city} - #{e.start.strftime('%m/%d/%y')}: #{e.name}",:length => 70), e.id]} \
+    | \
+    user.attending.reportable.newer_than(@calendar.past_event_cutoff - 60.days).all(:order=>"state,city").collect {|e| [truncate("#{e.state || e.country} - #{e.city} - #{e.start.strftime('%m/%d/%y')}: #{e.name}",:length => 70), e.id]}
   end
 end
